854403f8f7ce89a3133aba0db9f91183b9f3b574
DRaczek <damian.raczek4206695@gmail.com>
2025-10-02 00:38:36 +0200

    Created Utils file with forwarding observable properties methods

---
+++ Helpers/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace StepCounter.Helpers
+{
+    public class Utils
+    {
+        public void ForwardProperty(INotifyPropertyChanged source, string propertyName, Action<string> notifyTarget)
+        {
+            source.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == propertyName)
+                    notifyTarget(propertyName);
+            };
+        }
+        public void ForwardProperties(INotifyPropertyChanged source, IEnumerable<string> propertyNames, Action<string> notifyTarget)
+        {
+            foreach(string property in propertyNames)
+            {
+                ForwardProperty(source, property, notifyTarget);
+            }
+        }
+    }
+}
