be39673f09b10dfbc2d5117caecd5a9c7a4dc50e
DRaczek <damian.raczek4206695@gmail.com>
2025-10-01 21:28:31 +0200

    Created a simple pedometer history collection view

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,25 +1,39 @@
-using System;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Plugin.Maui.Pedometer;
+using StepCounter.Data;
+using StepCounter.Models;
 using StepCounter.Services;
+using System.Collections.ObjectModel;
 
 namespace StepCounter.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
         private readonly StepCounterService stepCounterService;
+        private readonly StepDatabase stepDatabase;
 
         public int DailySteps => stepCounterService.DailySteps;
 
-        public MainViewModel(StepCounterService stepCounterService)
+        [ObservableProperty]
+        public ObservableCollection<DailyStep> stepHistory = new();
+
+        public MainViewModel(StepCounterService stepCounterService, StepDatabase stepDatabase)
         {
             this.stepCounterService = stepCounterService;
+            this.stepDatabase = stepDatabase;
+
             stepCounterService.PropertyChanged += (s, e) =>
             {
                 if (e.PropertyName == nameof(StepCounterService.DailySteps))
                     OnPropertyChanged(nameof(DailySteps));
             };
+
+            _ = LoadHistoryAsync();
+        }
+
+        private async Task LoadHistoryAsync()
+        {
+            var steps = await stepDatabase.GetStepsAsync();
+            StepHistory = new ObservableCollection<DailyStep>(steps.Where(step => step.Date < DateTime.Today));
         }
     }
 }
