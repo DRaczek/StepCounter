466db86286f7d46a698491df8e57da771e3f919d
DRaczek <damian.raczek4206695@gmail.com>
2025-10-13 20:35:56 +0200

    forced dark theme

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@
         public App()
         {
             InitializeComponent();
+            Current.UserAppTheme = AppTheme.Dark;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
